737ce35ca4773213ba91487d32ac1ffa80fee38c
hannaalondra <85593312+hannaalondra@users.noreply.github.com>
2021-08-16 13:22:27 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: const Text('Welcome to Flutter'),
         ),
-        body: Center(
-          child: Text(''),
+        body: const Center(
+          child: Text('Hello World'),
         ),
-      );
+      ),
+    );
   }
 }
