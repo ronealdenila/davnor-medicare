0d4b10bdf901152c2a3c875c922397f0aed3bb0a
mizukageyama <emmy2699@gmail.com>
2021-08-16 12:41:13 +0800

    auth UI update

--- lib/main.dart
+++ lib/main.dart
@@ -33,39 +33,56 @@ class _AuthenticationState extends State<Authentication> {
       ),
       body: SingleChildScrollView(
         child: Column(children: <Widget>[
-          Padding(
-            //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-            padding: EdgeInsets.symmetric(horizontal: 15),
-            child: TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Email',
-                  hintText: 'Enter valid email id as abc@gmail.com'),
-            ),
+          Text(
+            'Welcome Back!',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
           ),
-          Padding(
-            padding: const EdgeInsets.only(
-                left: 15.0, right: 15.0, top: 15, bottom: 0),
-            //padding: EdgeInsets.symmetric(horizontal: 15),
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                  hintText: 'Enter secure password'),
+          TextField(
+            decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Email',
+                hintText: 'Enter valid email id as abc@gmail.com'),
+          ),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+                hintText: 'Enter secure password'),
+          ),
+          TextButton(
+            onPressed: () {
+              //TODO FORGOT PASSWORD SCREEN GOES HERE
+            },
+            child: Text(
+              'Forgot Password',
+              style: TextStyle(color: Colors.blue, fontSize: 15),
             ),
           ),
           Container(
             height: 50,
             width: 250,
             decoration: BoxDecoration(
-                color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+                color: Colors.blue[200],
+                borderRadius: BorderRadius.circular(10)),
             child: TextButton(
               onPressed: () {},
               child: Text(
-                'Login',
+                'Sign in',
                 style: TextStyle(color: Colors.white, fontSize: 25),
               ),
+            ),
+          ),
+          Text(
+            'Dont have an account?',
+          ),
+          TextButton(
+            onPressed: () {
+              //TODO FORGOT PASSWORD SCREEN GOES HERE
+            },
+            child: Text(
+              'Signuo here',
+              style: TextStyle(color: Colors.blue, fontSize: 15),
             ),
           ),
         ]),
