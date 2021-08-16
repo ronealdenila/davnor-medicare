b11b8cba99306e44a1b36179192def448f9f7e02
hannaalondra <85593312+hannaalondra@users.noreply.github.com>
2021-08-16 14:14:25 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -107,3 +107,37 @@ class _AuthenticationState extends State<Authentication> {
     ));
   }
 }
+class Termsandcondition extends StatefulWidget {
+  const Termsandcondition({ Key? key }) : super(key: key);
+
+  @override
+  _TermsandconditionState createState() => _TermsandconditionState();
+}
+
+class _TermsandconditionState extends State<Termsandcondition> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('Terms and Condition Page'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(2.0),
+          child: Column(children: <Widget>[
+            Center(
+              child: Text ('Terms and Condition'),
+            ),
+            Text(
+            'These terms and conditions outline the rules and regulators for the use of DavNor Medicare Multi-Platform Application. By accessing this application, we assume you accept these terms and conditions. Do not continue to use this application if you do not agree to take all of the terms and conditions stated on this page.',
+            style: TextStyle(fontWeight: FontWeight.normal, fontSize: kIsWeb ? 15 : 20),
+          ),
+
+          ],
+          )
+      ),
+      ),
+      );
+  }
+}
