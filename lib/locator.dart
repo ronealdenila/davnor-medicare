20fd44a05f95c73e591d1cab4fc98f1aff787741
Roneal Denila <ronealdenila271999@gmail.com>
2021-08-17 18:35:12 +0800

    Update locator.dart
    make a comment to explain nganong nag get it ko.
    
    PS: dili pani siya get na state management. get_it is for dependency  injection

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,6 +1,13 @@
 import 'package:davnor_medicare/core/services/authentication_service.dart';
 import 'package:get_it/get_it.dart';
 
+//Why I implemented this (R):
+//https://www.youtube.com/watch?v=vBT-FhgMaWM
+//"As your App grows, at some point you will need to put your app's logic
+// in classes that are separated from your Widgets. Keeping your widgets 
+//from having direct dependencies makes your code better organized and 
+//easier to test and maintain."
+
 GetIt locator = GetIt.instance;
 
 void setupLocator() {
