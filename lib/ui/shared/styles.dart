0d4148de216657edee76d6e855ea132b88495cc1
mizukageyama <emmy2699@gmail.com>
2021-08-21 17:07:23 +0800

    text styles added with Inter font

--- lib/ui/shared/styles.dart
+++ lib/ui/shared/styles.dart
@@ -1,7 +1,174 @@
 import 'package:flutter/material.dart';
 
-// const TextStyle titlesStyle
-const TextStyle heading1Style = TextStyle(
+// Titles
+const TextStyle title150Bold = TextStyle(
+  fontSize: 150,
+  fontWeight: FontWeight.w700,
+  fontFamily: 'Inter',
+);
+
+const TextStyle title58Regular = TextStyle(
+  fontSize: 58,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Inter',
+);
+
+const TextStyle title42Bold = TextStyle(
+  fontSize: 42,
+  fontWeight: FontWeight.w700,
+  fontFamily: 'Inter',
+);
+
+const TextStyle title40Regular = TextStyle(
+  fontSize: 40,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Inter',
+);
+
+const TextStyle title36Regular = TextStyle(
+  fontSize: 36,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Inter',
+);
+
+const TextStyle title34Regular = TextStyle(
   fontSize: 34,
   fontWeight: FontWeight.w400,
+  fontFamily: 'Inter',
+);
+
+const TextStyle title32Bold = TextStyle(
+  fontSize: 32,
+  fontWeight: FontWeight.w700,
+  fontFamily: 'Inter',
+);
+
+const TextStyle title32Regular = TextStyle(
+  fontSize: 32,
+  fontWeight: FontWeight.normal,
+  fontFamily: 'Inter',
+);
+
+const TextStyle title29Regular = TextStyle(
+  fontSize: 29,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Inter',
+);
+
+const TextStyle title24Bold = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+  fontFamily: 'Inter',
+);
+
+const TextStyle title24Medium = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w500,
+  fontFamily: 'Inter',
+);
+
+const TextStyle title24Regular = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Inter',
+);
+
+// Subtitles
+const TextStyle subtitle20Bold = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w700,
+  fontFamily: 'Inter',
+);
+
+const TextStyle subtitle20Medium = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w500,
+  fontFamily: 'Inter',
+);
+
+const TextStyle subtitle20Regular = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Inter',
+);
+
+const TextStyle subtitle18Bold = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w700,
+  fontFamily: 'Inter',
+);
+
+const TextStyle subtitle18Medium = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+  fontFamily: 'Inter',
+);
+
+const TextStyle subtitle18Regular = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Inter',
+);
+
+// Body
+const TextStyle body16SemiBold = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  fontFamily: 'Inter',
+);
+
+const TextStyle body16Regular = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Inter',
+);
+
+const TextStyle body16RegularUnderline = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  decoration: TextDecoration.underline,
+  fontFamily: 'Inter',
+);
+
+const TextStyle body14SemiBold = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+  fontFamily: 'Inter',
+);
+
+const TextStyle body14Medium = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  fontFamily: 'Inter',
+);
+
+const TextStyle body14Regular = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Inter',
+);
+
+// Captions
+const TextStyle caption12SemiBold = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w600,
+  fontFamily: 'Inter',
+);
+
+const TextStyle caption12Medium = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+  fontFamily: 'Inter',
+);
+
+const TextStyle caption12Regular = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Inter',
+);
+
+const TextStyle caption10Regular = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w400,
+  fontFamily: 'Inter',
 );
