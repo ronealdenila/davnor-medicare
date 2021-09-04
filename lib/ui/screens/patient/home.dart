eee6282b012cae8376a511fba1b816d5833616a6
Roneal Denila <ronealdenila271999@gmail.com>
2021-09-04 22:41:17 +0800

    rename prescription_model.dart

--- lib/ui/screens/patient/home.dart
+++ lib/ui/screens/patient/home.dart
@@ -2,7 +2,7 @@ import 'package:davnor_medicare/constants/app_strings.dart';
 import 'package:davnor_medicare/core/controllers/app_controller.dart';
 import 'package:davnor_medicare/core/controllers/auth_controller.dart';
 import 'package:davnor_medicare/core/controllers/cons_controller.dart';
-import 'package:davnor_medicare/core/models/prescription_model.dart';
+import 'package:davnor_medicare/core/models/consultation_model.dart';
 import 'package:davnor_medicare/core/services/article_service.dart';
 import 'package:davnor_medicare/helpers/dialogs.dart';
 import 'package:davnor_medicare/ui/screens/patient/cons_form.dart';
