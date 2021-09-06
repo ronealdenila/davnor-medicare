1a1a8a8205468fecd8d7628df70408c15d18d59e
hannaalondra <85593312+hannaalondra@users.noreply.github.com>
2021-09-06 15:55:04 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:davnor_medicare/core/controllers/bindings/all_controller_binding
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
+ await Firebase.initializeApp();
   runApp(MyApp());
 }
 
