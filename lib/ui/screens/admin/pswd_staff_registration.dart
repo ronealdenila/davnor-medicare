861f52f679181bcecf04a49c49cd46280b2a46d4
hannaalondra <85593312+hannaalondra@users.noreply.github.com>
2021-09-07 21:33:31 +0800

    done fixing admin(pswd and doctor registration

--- lib/ui/screens/admin/pswd_staff_registration.dart
+++ lib/ui/screens/admin/pswd_staff_registration.dart
@@ -1,187 +1,220 @@
 import 'package:davnor_medicare/constants/app_strings.dart';
+import 'package:davnor_medicare/ui/screens/admin/verification_req_item.dart';
 //import 'package:davnor_medicare/ui/shared/app_colors.dart';
 import 'package:davnor_medicare/ui/shared/styles.dart';
 import 'package:davnor_medicare/ui/shared/ui_helpers.dart';
 import 'package:davnor_medicare/ui/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class PSWDStaffRegistrationScreen extends StatelessWidget {
-  const PSWDStaffRegistrationScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       backgroundColor: Colors.white,
       body: Container(
-        width: 1400,
-        height: 550,
-        padding: const EdgeInsets.symmetric(horizontal: 200),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Row(children: [
-            Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const <Widget>[
-                  Text('PSWD Staff Registration Form ',
-                      textAlign: TextAlign.left, style: title24Bold),
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: SingleChildScrollView(child: ResponsiveView())),
+    );
+  }
+}
+
+class ResponsiveView extends GetResponsiveView {
+  ResponsiveView() : super(alwaysUseBuilder: false);
+
+  //TODO(R): Try to achieve phone and tablet must have same view
+  @override
+  Widget phone() => Column(
+        children: [
+          SizedBox(
+            height: Get.height,
+            width: Get.width,
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              pswdInfo(),
+              verticalSpace35,
+              Column(
+                children: <Widget>[
+                  regPSWDInfo(),
                   verticalSpace35,
-                  Text(pswdStaffRegister,
-                      textAlign: TextAlign.left, style: body16SemiBold),
-                ])
-          ]),
-          verticalSpace10,
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              horizontalSpace20,
-              SizedBox(
-                width: screenWidthPercentage(context, percentage: .3),
-                child: const Text(
-                  'Last Name',
-                  style: body14Regular,
-                ),
+                  regPSWDInfop(),
+                ],
               ),
-              horizontalSpace20,
-              SizedBox(
-                width: screenWidthPercentage(context, percentage: .3),
-                child: const Text(
-                  'Position',
-                  style: body14Regular,
-                ),
+              verticalSpace35,
+              screenButtons()
+            ]),
+          )
+        ],
+      );
+
+  @override
+  Widget tablet() => Column(
+        children: [
+          SizedBox(
+            height: Get.height,
+            width: Get.width,
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              pswdInfo(),
+              verticalSpace35,
+              Column(
+                children: <Widget>[
+                  regPSWDInfo(),
+                  verticalSpace35,
+                  regPSWDInfop(),
+                ],
               ),
-            ],
+              verticalSpace35,
+              screenButtons()
+            ]),
+          )
+        ],
+      );
+
+  @override
+  Widget desktop() => Column(
+        children: [
+          SizedBox(
+            height: Get.height,
+            width: screen.width,
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              pswdInfo(),
+              verticalSpace35,
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  regPSWDInfo(),
+                  horizontalSpace80,
+                  regPSWDInfop(),
+                ],
+              ),
+              verticalSpace35,
+              screenButtons()
+            ]),
           ),
-          verticalSpace10,
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              horizontalSpace20,
-              SizedBox(
-                width: screenWidthPercentage(context, percentage: .3),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
-                      ),
-                    ),
-                  ),
-                ),
+        ],
+      );
+
+  Widget screenButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        CustomButton(
+          onTap: () async {},
+          text: 'ADD',
+          buttonColor: Colors.blue[900],
+          fontSize: 15,
+        ),
+        horizontalSpace40,
+        CustomButton(
+          onTap: () async {},
+          text: 'Cancel',
+          buttonColor: Colors.blue[900],
+          fontSize: 15,
+        ),
+      ],
+    );
+  }
+
+  Widget regPSWDInfo() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Last Name',
+          style: body14Medium,
+        ),
+        verticalSpace15,
+        SizedBox(
+        width: 340,
+        height: 90,
+        child: TextFormField(
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
               ),
-              horizontalSpace20,
-              SizedBox(
-                width: screenWidthPercentage(context, percentage: .3),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
-          verticalSpace10,
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              horizontalSpace35,
-              SizedBox(
-                width: screenWidthPercentage(context, percentage: .3),
-                child: const Text(
-                  'First Name',
-                  style: body14Regular,
-                ),
-              ),
-              horizontalSpace15,
-            ],
-          ),
-          verticalSpace10,
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              horizontalSpace35,
-              SizedBox(
-                width: screenWidthPercentage(context, percentage: .3),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              horizontalSpace10,
-            ],
-          ),
-          verticalSpace10,
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              horizontalSpace35,
-              SizedBox(
-                width: screenWidthPercentage(context, percentage: .3),
-                child: const Text(
-                  'Email Address',
-                  style: body14Regular,
-                ),
-              ),
-              horizontalSpace15,
-            ],
-          ),
-          verticalSpace10,
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              horizontalSpace35,
-              SizedBox(
-                width: screenWidthPercentage(context, percentage: .3),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              horizontalSpace10,
-            ],
-          ),
-          verticalSpace35,
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Align(
-                alignment: FractionalOffset.bottomRight,
-                child: CustomButton(
-                  onTap: () async {},
-                  text: 'ADD',
-                  buttonColor: Colors.blue[900],
-                  fontSize: 20,
-                ),
-              ),
-              horizontalSpace40,
-              Align(
-                alignment: FractionalOffset.bottomRight,
-                child: CustomButton(
-                  onTap: () async {},
-                  text: 'Cancel',
-                  buttonColor: Colors.blue[900],
-                  fontSize: 20,
-                ),
-              ),
-            ],
-          ),
-        ]),
+        ),
       ),
+
+      const Text(
+          'First Name',
+          style: body14Medium,
+        ),
+        verticalSpace15,
+        SizedBox(
+        width: 340,
+        height: 90,
+        child: TextFormField(
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+          ),
+        ),
+      ),
+      const Text(
+          'Email Address',
+          style: body14Medium,
+        ),
+        verticalSpace15,
+        SizedBox(
+        width: 340,
+        height: 90,
+        child: TextFormField(
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+          ),
+        ),
+      ),
+      ]);
+  }  
+
+    Widget regPSWDInfop() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Position',
+          style: body14Medium,
+        ),
+        verticalSpace15,
+        SizedBox(
+        width: 340,
+        height: 90,
+        child: TextFormField(
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+          ),
+        ),
+      ),
+      ]);
+  }
+
+  Widget pswdInfo() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const[
+         Text('PSWD Staff Registration Form ',
+        textAlign: TextAlign.left, style: title24Bold),
+        verticalSpace10,
+         Text(pswdStaffRegister,
+        textAlign: TextAlign.left, style: body16SemiBold),
+      ]
     );
   }
 }
