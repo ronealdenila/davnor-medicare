89c15bd88697618752b0514f091a882eebeca20e
mizukageyama <63276829+mizukageyama@users.noreply.github.com>
2021-09-10 20:55:09 +0800

    Paginated MA Queue Table

--- lib/ui/screens/patient/queue_ma_table.dart
+++ lib/ui/screens/patient/queue_ma_table.dart
@@ -3,89 +3,78 @@ import 'package:davnor_medicare/ui/shared/app_colors.dart';
 // import 'package:davnor_medicare/ui/shared/ui_helpers.dart';
 
 import 'package:davnor_medicare/core/controllers/patient/ma_queue_controller.dart';
+import 'package:davnor_medicare/ui/shared/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:shimmer/shimmer.dart';
 
 class QueueMATableScreen extends StatelessWidget {
   final MAQueueController maQueueController = Get.put(MAQueueController());
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(),
-        body: SizedBox(
-            width: Get.width,
-            child: StreamBuilder(
-                stream: maQueueController.getMAQueueCollection(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.active) {
-                    return getDataTable();
-                  } else if (snapshot.connectionState == ConnectionState.none) {
-                    return dataTableNoData();
-                  }
-                  return loadingDataTable();
-                })));
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(),
+          body: SingleChildScrollView(
+            child: SizedBox(
+                width: Get.width,
+                child: StreamBuilder(
+                    stream: maQueueController.getMAQueueCollection(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.active) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 10),
+                          child: getDataTable(context),
+                        );
+                      } else if (snapshot.connectionState ==
+                          ConnectionState.none) {
+                        return noData();
+                      }
+                      return loadingIndicator();
+                    })),
+          )),
+    );
   }
 
-  Widget getDataTable() {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-        child: DataTable(
-            headingTextStyle: const TextStyle(color: Colors.white),
-            showBottomBorder: true,
-            headingRowColor: MaterialStateColor.resolveWith(
-                (states) => Colors.blue.shade200),
-            columns: const [
-              DataColumn(label: Text('No.')),
-              DataColumn(label: Text('Queue No.')),
-            ],
-            rows: List<DataRow>.generate(
-                20,
-                (index) => DataRow(cells: [
-                      DataCell(Text('${index + 1}')),
-                      DataCell(
-                          Text(maQueueController.queueMAList[index].queueNum!)),
-                    ]))),
+  Widget getDataTable(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        cardTheme: CardTheme(
+          elevation: 4,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          clipBehavior: Clip.antiAlias,
+        ),
+        dataTableTheme: DataTableThemeData(
+          headingRowColor: MaterialStateProperty.all(Colors.blue.shade200),
+        ),
+        iconTheme: const IconThemeData(color: Colors.blue),
+      ),
+      child: SizedBox(
+        width: Get.width,
+        child: PaginatedDataTable(
+          source: QueueDataSource(queueMAList: maQueueController.queueMAList),
+          columns: const [
+            DataColumn(label: Text('No.', style: subtitle18BoldWhite)),
+            DataColumn(label: Text('Queue No.', style: subtitle18BoldWhite)),
+          ],
+        ),
       ),
     );
   }
 
-  Widget loadingDataTable() {
-    return Shimmer.fromColors(
-      baseColor: neutralColor[10]!,
-      highlightColor: Colors.white,
-      child: DataTable(
-          columns: const [
-            DataColumn(label: Text('No.')),
-            DataColumn(label: Text('Queue No.')),
-          ],
-          rows: List<DataRow>.generate(
-              20,
-              (index) => const DataRow(cells: [
-                    DataCell(Text('')),
-                    DataCell(Text('')),
-                  ]))),
+  Widget loadingIndicator() {
+    return const Center(
+      child: SizedBox(
+        width: 20,
+        height: 20,
+        child: CircularProgressIndicator(),
+      ),
     );
   }
 
-  Widget dataTableNoData() {
-    return Column(
-      children: [
-        DataTable(
-            columns: const [
-              DataColumn(label: Text('No.')),
-              DataColumn(label: Text('Queue No.')),
-            ],
-            rows: List<DataRow>.generate(
-                1,
-                (index) => const DataRow(cells: [
-                      DataCell(Text('')),
-                      DataCell(Text('')),
-                    ]))),
-        const Text('There are no queue')
-      ],
-    );
+  Widget noData() {
+    return const Text('There are no queue');
   }
 }
