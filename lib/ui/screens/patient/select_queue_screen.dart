ce336c0d13d72e3568058dcb1fb98505ad41efdf
hannaalondra <85593312+hannaalondra@users.noreply.github.com>
2021-11-01 20:05:17 +0800

    Update: Back Button

--- lib/ui/screens/patient/select_queue_screen.dart
+++ lib/ui/screens/patient/select_queue_screen.dart
@@ -2,15 +2,23 @@ import 'package:davnor_medicare/ui/shared/app_colors.dart';
 import 'package:davnor_medicare/ui/shared/styles.dart';
 import 'package:davnor_medicare/ui/widgets/patient/patient_custom_button.dart';
 import 'package:davnor_medicare_ui/davnor_medicare_ui.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SelectQueueScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.chevron_left),
+          onPressed: () {},
+        color: Colors.black,
+        ),
+      ),
+      
       body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 60, horizontal: 30),
+        padding: const EdgeInsets.symmetric( vertical: 60, horizontal: 30),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
